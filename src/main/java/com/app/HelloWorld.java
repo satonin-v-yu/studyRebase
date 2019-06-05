d0b929fe9d5d1ab5a5f322ff01e64c937e917362
satonin18 <satonin18@mail.ru>
2019-06-05 11:04:41 +0500

    add Other 1

--- src/main/java/com/app/HelloWorld.java
+++ src/main/java/com/app/HelloWorld.java
@@ -5,5 +5,10 @@ import java.io.*;
 public class HelloWorld {
 
     public static void main(String[] args) throws IOException {
+<<<<<<< HEAD
 	System.out.println("Hello every body");
+=======
+	System.out.println("Other 1 ");
+	System.out.println("Hello ");
+>>>>>>> add Other 1
 }
