58b9ff74a38630426a0b91e785631131f22636dc
satonin18 <35444999+satonin18@users.noreply.github.com>
2019-06-05 10:30:55 +0500

    //1
    //2

---
+++ src/main/java/com/app/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.app;
+
+import java.io.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) throws IOException {
+	System.out.println("Hello every body");
+	System.out.println("Other 2 ");
+}
