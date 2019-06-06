d1cbc5bf582ef2222490ca800bf60e3a41e4692a
satonin18 <satonin18@mail.ru>
2019-06-06 09:33:40 +0500

    3

--- src/main/java/com/app/HelloWorld.java
+++ src/main/java/com/app/HelloWorld.java
@@ -7,5 +7,5 @@ public class HelloWorld {
     public static void main(String[] args) throws IOException {
 	System.out.println("1");
 	System.out.println("2");
-	System.out.println("BYE");
+	System.out.println("3");
 }
