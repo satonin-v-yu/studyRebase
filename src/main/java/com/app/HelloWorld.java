d61608224861715ba4d66e814e891b3ccfde8241
satonin18 <satonin18@mail.ru>
2019-06-06 09:32:49 +0500

    1

--- src/main/java/com/app/HelloWorld.java
+++ src/main/java/com/app/HelloWorld.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class HelloWorld {
 
     public static void main(String[] args) throws IOException {
-	System.out.println("BYE");
+	System.out.println("1");
 	System.out.println("BYE");
 	System.out.println("BYE");
 }
